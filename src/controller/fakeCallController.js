4fd2ff8713f95dc59e4fe0eb0a93bb34d3b40836
Abhinav MS <abhinavms@outlook.com>
2021-10-23 15:46:13 +0530

    Completed Fake Call

--- src/controller/fakeCallController.js
+++ src/controller/fakeCallController.js
@@ -1,15 +1,22 @@
 const userCollection = require('../app').collection("Users");
 
+const twilio = require('../config').twilio;
+const client = require('twilio')(twilio.accountSid, twilio.authToken);
+
 exports.fakeCall = async (req, res) => {
     const id = req.body.user;
     const user = await userCollection.findOne({_id: id});
     
     if (user) {
-        contacts = user.contacts;
-        contacts.forEach(async contact => {
-            console.log("Sending SOS to " , contact.name, " at ", contact.phoneno);
+        client.calls.create({
+         url: 'http://demo.twilio.com/docs/voice.xml',
+         to: "+91" + user._id,
+         from: '+12058469725'
+       })
+      .then(call => {
+          console.log(call);
+          res.status(200).send("Fake Call sent");
         });
-        res.status(200).send("Fake Call sent");
     }
     else {
         res.status(400).send("User not found");
